f3fde2963c63e8ae2f7f4df68b9dcf892465a35d
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-11-02 12:31:10 +0700

    Add CLI for migrations and start/stop servers.

--- main.go
+++ main.go
@@ -3,15 +3,52 @@ package main
 import (
 	"fmt"
 
+	"github.com/urfave/cli"
 	"micro-auth/api"
 	"micro-auth/config"
 	"micro-auth/migrations"
+	"os"
+	"runtime"
 )
 
 func main() {
 	config.Load()
 
-	migrations.Up(config.App.Database.Name, config.App.Database.Dialect, config.App.Database.MigrationsDir)
 	server := api.Server()
-	server.Run(fmt.Sprintf(":%d", config.App.Server.Port))
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	clientApp := cli.NewApp()
+	clientApp.Name = "Maps API"
+	clientApp.Version = "0.0.1"
+	clientApp.Commands = []cli.Command{
+		{
+			Name:        "start",
+			Description: "Start Http Server",
+			Action: func(c *cli.Context) error {
+				server.Run(fmt.Sprintf(":%d", config.App.Server.Port))
+				return nil
+			},
+		},
+		{
+			Name:        "db:migrate:up",
+			Description: "Start Http Server",
+			Action: func(c *cli.Context) error {
+				migrations.Up(config.App.Database.Name, config.App.Database.Dialect, config.App.Database.MigrationsDir)
+				return nil
+			},
+		},
+		{
+			Name:        "db:migrate:down",
+			Description: "Start Http Server",
+			Action: func(c *cli.Context) error {
+				migrations.Down(config.App.Database.Name, config.App.Database.Dialect, config.App.Database.MigrationsDir)
+				return nil
+			},
+		},
+	}
+	if err := clientApp.Run(os.Args); err != nil {
+		panic(err)
+	}
+
 }
--- migrations/run.go
+++ migrations/run.go
@@ -20,3 +20,16 @@ func Up(dbname string, dialect string, migrationsDir string) {
 
 	migrate.Exec(db, dialect, migrations, migrate.Up)
 }
+
+func Down(dbname string, dialect string, migrationsDir string) {
+	dbinfo := fmt.Sprintf("dbname=%s sslmode=disable", dbname)
+	db, err := sql.Open(dialect, dbinfo)
+	if err != nil {
+		fmt.Printf("sql.Open failed due to: %s", err.Error())
+	}
+	migrations := &migrate.FileMigrationSource{
+		Dir: migrationsDir,
+	}
+
+	migrate.Exec(db, dialect, migrations, migrate.Down)
+}
